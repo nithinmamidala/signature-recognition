6f08264beaefc4efd4213f21d0a5c13d557160f4
nithin <nithinmamidala.1@gmail.com>
2026-04-10 01:40:00 +0530

    jupyter-notebood-done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='src',
+    version='0.1.0',
+    author="nithin",
+    author_email="nithinmamidala.1@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
